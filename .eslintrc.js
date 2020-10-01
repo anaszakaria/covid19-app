8cb3b7ed896fcaeba01a4cfd3c09aa95aedcca1e
Anas Zakaria <anaszcm@gmail.com>
2020-10-02 00:30:22 +0800

    Init covid19-app with Vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true
     },
-  ],
-};
+    extends: ['plugin:vue/essential', '@vue/airbnb'],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        quotes: ['error', 'single', 'avoid-escape'],
+        semi: [2, 'never'],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'linebreak-style': 'off',
+        'comma-dangle': ['error', 'never'],
+        'no-cond-assign': ['error', 'always'],
+        'no-param-reassign': 'off',
+        'arrow-body-style': ['error', 'as-needed'],
+        'max-len': [2, { code: 120, tabWidth: 4, ignoreUrls: true }],
+        'no-restricted-syntax': 'off',
+        'no-unused-vars': 'off'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App)
+}).$mount('#app')
