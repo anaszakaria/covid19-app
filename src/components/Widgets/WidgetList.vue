32ed98383cd7d235a3f1b6552302511c209fa05b
Anas Zakaria <anaszcm@gmail.com>
2020-12-04 14:55:24 +0800

    Added wiget options switch to Profile page

---
+++ src/components/Widgets/WidgetList.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card :dark="isDark" :color="widgetColor">
+        <v-card-title>
+            <v-icon medium left>
+                {{ icon }}
+            </v-icon>
+            <span class="title font-weight-light">{{ title }}</span>
+        </v-card-title>
+        <v-card-text v-if="total" class="headline font-weight-bold text-center">
+            {{ total | numberWithCommas }}
+        </v-card-text>
+        <v-card-text v-else class="text-center">
+            <v-progress-circular v-if="preLoader" indeterminate color="primary"></v-progress-circular>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        widgetColor: String,
+        total: [Number, String],
+        title: String,
+        icon: String,
+        isDark: {
+            type: Boolean,
+            default: true
+        },
+        preLoader: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            totalValue: ''
+        }
+    },
+    watch: {
+        total(value) {
+            if (!value) {
+                this.total = 'N/A'
+            }
+        }
+    }
+}
+</script>
--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -1,10 +1,39 @@
 <template>
     <v-container fluid>
+        <h3>User Profile</h3>
         <v-row class="ma-0">
             <v-col sm="12">
-                <h3>User Profile</h3>
-                <v-card outlined tile elevation="2" class="pa-4">
-                    User Profile Content
+                <v-card outlined elevation="1">
+                    <v-list dense subheader>
+                        <v-subheader class="subtitle-2">Dashboard Widget</v-subheader>
+                        <v-list-item v-for="(widget, index) in dashboardWidget" :key="index">
+                            <v-list-item-action>
+                                <v-switch v-model="widget.enabled"></v-switch>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title class="subtitle-2 font-weight-regular">{{
+                                    widget.title
+                                }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-col>
+            <v-col sm="12">
+                <v-card outlined elevation="1">
+                    <v-list dense subheader>
+                        <v-subheader class="subtitle-2">Country Statistics Widget</v-subheader>
+                        <v-list-item v-for="(widget, index) in countryStatisticWidget" :key="index">
+                            <v-list-item-action>
+                                <v-switch v-model="widget.enabled"></v-switch>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title class="subtitle-2 font-weight-regular">{{
+                                    widget.title
+                                }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
                 </v-card>
             </v-col>
         </v-row>
@@ -14,7 +43,44 @@
 <script>
 export default {
     data() {
-        return {}
+        return {
+            dashboardWidget: [
+                { title: 'Summary', enabled: true },
+                { title: 'Data Comparison Between Active, Recovered and Death Cases', enabled: true },
+                { title: 'Confirmed Cases', enabled: true },
+                { title: 'Percentage of Active, Recovered and Death Cases', enabled: true },
+                { title: 'Active Cases', enabled: true },
+                { title: 'Cases Recovered', enabled: true },
+                { title: 'Death Cases', enabled: true }
+            ],
+            countryStatisticWidget: [
+                { title: 'Summary', enabled: true },
+                { title: 'Cases Per Million Pie Chart', enabled: true },
+                { title: 'Deaths Per Million Pie Chart', enabled: true },
+                { title: 'Tests Per Million Pie Chart', enabled: true },
+                { title: 'Trending Data Line Chart', enabled: true }
+            ]
+        }
+    },
+    watch: {
+        dashboardWidget: {
+            handler(value) {
+                console.log(value)
+            },
+            deep: true
+        },
+        countryStatisticWidget: {
+            handler(value) {
+                console.log(value)
+            },
+            deep: true
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.v-label {
+    left: 12px !important;
+}
+</style>
