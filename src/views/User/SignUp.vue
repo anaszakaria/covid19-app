1beccc805f81a235663e9b78747a4be995458d85
Anas Zakaria <anaszcm@gmail.com>
2020-12-20 03:30:03 +0800

    Added savedCountry to user signup

--- src/views/User/SignUp.vue
+++ src/views/User/SignUp.vue
@@ -94,6 +94,7 @@ export default {
                 name: this.name,
                 email: this.email,
                 password: this.password,
+                savedCountry: 'Malaysia',
                 widget: this.$store.getters.widget
             })
         },
