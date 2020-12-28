569f91c0ab80729eb04d343ac89aa3988d43f692
Anas Zakaria <anaszcm@gmail.com>
2020-12-28 19:08:08 +0800

    Updated World Statistics title

--- src/views/Stats/Statistics.vue
+++ src/views/Stats/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid>
-        <h4 class="font-weight-medium ml-3 mb-1">COVID-19 Statistics for Countries - Updated on {{ lastUpdatedOn }}</h4>
+        <h4 class="font-weight-medium ml-3 mb-1">World COVID-19 Statistics - Updated on {{ lastUpdatedOn }}</h4>
         <v-row class="ma-0">
             <v-col sm="12">
                 <v-card outlined tile>
